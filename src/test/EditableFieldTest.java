d5342ba726a44c7a5e7932cb6b9297cada0c3c10
Shyshkin Vladyslav <shyshkin.vladyslav@gmail.com>
2017-05-20 23:05:59 +0300

    improved some test

--- src/test/EditableFieldTest.java
+++ src/test/EditableFieldTest.java
@@ -11,6 +11,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -34,7 +35,7 @@ public class EditableFieldTest {
      * test pattern for phone
      */
     @Test
-    public void patternTestPhone(){
+    public void patternTestPhone() {
         Pattern p = Pattern.compile("^((\\+380|0)([0-9]{9}))?$");
         Matcher m = p.matcher("0913030595");
         assertTrue(m.matches());
@@ -44,19 +45,31 @@ public class EditableFieldTest {
      * test pattern for email
      */
     @Test
-    public void patternTestTextEmail(){
+    public void patternTestTextEmail() {
         Pattern p = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         Matcher m = p.matcher("marinas0595@gmail.com");
         assertTrue(m.matches());
+
+        m = p.matcher("shyslav@gmail.com");
+        assertTrue(m.matches());
+
+        m = p.matcher("shyshin.vladyslav@gmail.com");
+        assertTrue(m.matches());
+
+        m = p.matcher("agmail.com");
+        assertFalse(m.matches());
     }
 
     /**
      * test pattern for textfield
      */
     @Test
-    public void patternTestText(){
+    public void patternTestText() {
         Pattern p = Pattern.compile("[\\d\\w\\t\\n\\r\\f\\v\\W*.]{3,50}");
         Matcher m = p.matcher("some text любой текст+-.,/;:_");
+        assertTrue(m.matches());
+
+        m = p.matcher("123 213 214 test тест 321");
         assertTrue(m.matches());
     }
 
@@ -64,27 +77,35 @@ public class EditableFieldTest {
      * test pattern for textarea
      */
     @Test
-    public void patternTestTextArea(){
+    public void patternTestTextArea() {
         Pattern p = Pattern.compile("[\\d\\w\\t\\n\\r\\f\\v\\W*.]{3,16777215}");
         Matcher m = p.matcher("some text любой текст+-.,/;:_");
         assertTrue(m.matches());
     }
+
     /**
      * test pattern for numbers
      */
     @Test
-    public void patternTestNumber(){
+    public void patternTestNumber() {
         Pattern p = Pattern.compile("[\\d]{1,11}");
         Matcher m = p.matcher("1000000");
         assertTrue(m.matches());
     }
+
     /**
      * test pattern for double numbers
      */
     @Test
-    public void patternTestNumberDouble(){
+    public void patternTestNumberDouble() {
         Pattern p = Pattern.compile("^\\d*\\.?\\d*$");
         Matcher m = p.matcher("1000000.0");
+        assertTrue(m.matches());
+
+        m = p.matcher("5.0");
+        assertTrue(m.matches());
+
+        m = p.matcher("3.932");
         assertTrue(m.matches());
     }
 
